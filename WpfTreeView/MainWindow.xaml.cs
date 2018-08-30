d91f9eb7843d43f45d85d30dc975901cb95e562a
CoderCallender <42542281+CoderCallender@users.noreply.github.com>
2018-08-30 20:08:46 +0100

    Finds drives and subfolders
    up to 42 mins in the video

--- WpfTreeView/MainWindow.xaml.cs
+++ WpfTreeView/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfTreeView
 {
@@ -45,15 +34,110 @@ namespace WpfTreeView
         /// <param name="e"></param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //Get every logical drive on the machine 
            foreach ( var drive in Directory.GetLogicalDrives())
             {
+                //create a new item for it
                 var item = new TreeViewItem();
 
+                //Set the header and path
                 item.Header = drive;
+                item.Tag = drive;
 
+                //Add a dummy item
+                item.Items.Add(null);
+
+                //Listen out for the item being expanded
+                item.Expanded += Folder_Expanded;
+
+                //Add it to the main tree view
                 FolderView.Items.Add(item);
             }
         }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            //Check if the item contains the dummy data
+            if((item.Items.Count != 1) || (item.Items[0] != null))
+            {
+                //if it isn't, then return
+                return;
+            }
+
+            //Clear dummy data 
+            item.Items.Clear();
+
+            //get the full path
+            var fullPath = (string)item.Tag;
+
+            //Create a blanks list
+            var directories = new List<string>();
+
+            //Try and get directories from the folder
+            //ignoring any issues that we encounter
+            try
+            {
+                
+                var dirs = Directory.GetDirectories(fullPath);
+
+                //try and 
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+            }
+
+            catch { }
+
+            //For each directory...
+            directories.ForEach(directoryPath =>
+            {
+                //Create directory item
+                var subItem = new TreeViewItem()
+                {
+                    //set folder name
+                    Header = GetFileFolderName(directoryPath),
+                    //set folder path
+                    Tag = directoryPath
+                };
+
+                //Add dummy item so we can expand the view
+                subItem.Items.Add(null);
+
+                //calls this function again if folder is expanded
+                subItem.Expanded += Folder_Expanded;
+
+                //add items to the parent
+                item.Items.Add(subItem);
+            });
+
+
+        }
         #endregion
+
+        /// <summary>
+        /// Find the file or folder name from a ful path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string GetFileFolderName(string path)
+        {
+            //if there's nothing in the string, return empty
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            //make all slahes backslashes
+            var normalisedPath = path.Replace('/','\\');
+
+            //find the last backslash of any path
+            var lastIndex = normalisedPath.LastIndexOf('\\');
+
+            //if we don't find a backslash, return the path itself
+            if (lastIndex <= 0)
+                return path;
+
+            //return the name after the last backslash
+            return path.Substring(lastIndex + 1); 
+        }
     }
 }
